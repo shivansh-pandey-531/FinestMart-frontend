f7ac83297d0f81df83a4b61199bc90d43f5b1e9b
shivansh-pandey-531 <pandey.shivansh@webseeder.tech>
2025-06-25 16:23:02 +0530

    initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ import Footer from "./components/Footer";
 function App() {
 
   return (
-    <div className="sm:px-2 sm:py-2 md:px-16 md:py-4 w-screen h-screen">
+    <div className="sm:px-8 sm:py-2 md:px-16 md:py-4 w-screen h-screen">
       <Navbar />
       <HeroSection />
       <Footer />
